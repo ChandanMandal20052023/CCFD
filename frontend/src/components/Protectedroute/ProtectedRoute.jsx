8a4f155a08206f99b682781d3fba21a5759d7234
Chandan Mandal <149592316+ChandanMandal20052023@users.noreply.github.com>
2026-03-27 03:35:09 +0530

    Delete frontend directory

--- frontend/src/components/Protectedroute/ProtectedRoute.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Navigate, Outlet } from 'react-router-dom';
-const ProtectedRoute = () => {
-  // Check karo ki token localStorage mein hai ya nahi
-  const token = localStorage.getItem('token');
-  return token ? <Outlet /> : <Navigate to="/login" replace />;
-};
-
-export default ProtectedRoute;
